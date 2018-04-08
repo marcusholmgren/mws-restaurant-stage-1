8f9243ee685304074f36b3ca2d1ea07c21fe573c
Marcus Holmgren <marcus.holmgren1@gmail.com>
2018-04-08 19:40:55 +0200

    feat: Restaurant images have alt tags

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,6 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `Restaurant ${restaurant.name} in ${restaurant.neighborhood}`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
